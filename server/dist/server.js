2b0c01f80ce8e9a305fbaf006d759b9fb3caa8d4
Benjamin Machock <43613715+benjaminmachock@users.noreply.github.com>
2025-02-25 23:04:05 -0700

    test

---
+++ server/dist/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { ApolloServer } from "@apollo/server"; // Note: Import from @apollo/server-express
+import { expressMiddleware } from "@apollo/server/express4";
+import { typeDefs, resolvers } from "./schemas/index.js";
+import { authenticateToken } from "./services/auth.js";
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+const startApolloServer = async () => {
+    await server.start();
+    const app = express();
+    const PORT = process.env.PORT || 3001;
+    //middleware
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use("/graphql", expressMiddleware(server, {
+        context: authenticateToken,
+    }));
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+};
+startApolloServer();
