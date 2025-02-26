7a7151e6468d87b89bcd9ea9498106b510f2c413
Benjamin Machock <43613715+benjaminmachock@users.noreply.github.com>
2025-02-25 23:54:15 -0700

    test

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import path from "node:path";
 import { ApolloServer } from "@apollo/server"; // Note: Import from @apollo/server-express
 import { expressMiddleware } from "@apollo/server/express4";
 import { typeDefs, resolvers } from "./schemas/index.js";
@@ -18,12 +17,6 @@ const startApolloServer = async () => {
     app.use("/graphql", expressMiddleware(server, {
         context: authenticateToken,
     }));
-    if (process.env.NODE_ENV === "production") {
-        app.use(express.static(path.join(__dirname, "../client/dist")));
-        app.get("*", (_req, res) => {
-            res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-        });
-    }
     app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}!`);
         console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
