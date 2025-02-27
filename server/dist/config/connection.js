e037d30da0f82f70535f3873f080ec19c7d14f63
Benjamin Machock <43613715+benjaminmachock@users.noreply.github.com>
2025-02-27 11:57:15 -0700

    test

--- server/dist/config/connection.js
+++ server/dist/config/connection.js
@@ -1,3 +1,4 @@
-import mongoose from 'mongoose';
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
-export default mongoose.connection;
+import mongoose from "mongoose";
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks");
+const db = mongoose.connect;
+export default db;
